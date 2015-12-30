2add0a6b0e1c324a12155e5fe8ce67c0f083a9cf
Groskilled <adam.wybierala@gmail.com>
2015-12-30 14:26:32 +0000

    recuperation des data ok, modifier la dimension du vector cree

--- Network.py
+++ Network.py
@@ -6,15 +6,31 @@ class Network(object):
         ret = {}
         for i in range(len(sizes[1:])):
             ret[i] = np.random.randn(sizes[i + 1], 1)
-            print ret[i].shape
         return ret;
 
     def init_weights(self, sizes):
         ret = {}
         for i in range(len(sizes) - 1):
             ret[i] = np.random.randn(sizes[i], sizes[i + 1])
-            print ret[i].shape
         return ret
+
+    def feedforward(self, a):
+        for n in range(self.n_lay):
+            a = np.dot(self.weights[n], a)
+
+    def gradient_descent(self, data, size, max_iter, lrate):
+        '''
+        data is the whole set of data
+        size is the desired size for the training set
+        max_iter is the number of time we want to train
+        lrate is the learning rate
+        '''
+        n = len(data)
+        for i in range(max_iter):
+            random.shuffle(data)
+            mini_set = np.array(training_data[k:k+size] for k in xrange(0, n, size))
+            print mini_set.shape
+            print "Cycle number {0} complete.".format(i)
     
     def __init__(self, sizes):
         self.n_lay = len(sizes)
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import numpy as np
 import random
 import Network
+import scipy.io as sio
 
 net = Network.Network([400, 30, 10])
+data = sio.loadmat('ex4data1.mat')
+X = np.array(data['X'])
+Y = np.array(data['y'])
+y_ = []
+print np.array([[9 == j] for j in range(10)], dtype=int)
+#for i in range(len(Y)):
+#    y.append()
+#net.gradient_descent(self, data, size, max_iter, lrate):
