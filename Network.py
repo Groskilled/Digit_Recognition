e9659c42d836ae370a14c75677fe42f0bf5a9030
Groskilled <adam.wybierala@gmail.com>
2016-01-03 12:29:55 +0000

    correction in sigmoid, adding cost function and feedforward prop

--- Network.py
+++ Network.py
@@ -1,4 +1,7 @@
 import numpy as np
+import random
+import time
+import functions
 
 class Network(object):
 
@@ -15,8 +18,19 @@ class Network(object):
         return ret
 
     def feedforward(self, a):
-        for n in range(self.n_lay):
-            a = np.dot(self.weights[n], a)
+        for n in range(self.n_lay - 1):
+            a = functions.sigmoid(np.dot(self.weights[n].T, a) + self.biases[n])
+        return a
+
+    def cost_function(self, X):
+        cost = 0
+        for ex in X:
+            est = self.feedforward(np.reshape(ex[0:400], (400, 1)))
+            left = np.reshape(ex[400:410], (10, 1)) - np.log(est)
+            right = (1 - np.reshape(ex[400:410], (10, 1))) * np.log(1 - est)
+            cost = cost + np.sum(left - right) / 100
+        cost = cost / X.shape[0]
+        return cost
 
     def gradient_descent(self, data, size, max_iter, lrate):
         '''
@@ -28,8 +42,8 @@ class Network(object):
         n = len(data)
         for i in range(max_iter):
             random.shuffle(data)
-            mini_set = np.array(training_data[k:k+size] for k in xrange(0, n, size))
-            print mini_set.shape
+            mini_batch = np.array([data[k] for k in range(size)])
+            self.cost_function(mini_batch)
             print "Cycle number {0} complete.".format(i)
     
     def __init__(self, sizes):
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ data = sio.loadmat('ex4data1.mat')
 X = np.array(data['X'])
 Y = np.array(data['y'])
 y_ = []
-#print np.array([[1 == j] for j in range(10)], dtype=int).T[0]
 for i in range(len(Y)):
     y_.append(np.array([[Y[i] % 10 == j] for j in range(10)], dtype=int).T[0][0])
 y_ = np.array(y_)
-#net.gradient_descent(self, data, size, max_iter, lrate):
+X = np.c_[X, y_]
+net.gradient_descent(X, 400, 3, 0.1)
