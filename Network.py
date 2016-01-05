c0b6a6c820a324a221555ed4a142ee97ff8cf11f
Groskilled <adam.wybierala@gmail.com>
2016-01-05 18:51:48 +0000

    problem with the sizes of the activation, compare with octave

--- Network.py
+++ Network.py
@@ -36,24 +36,30 @@ class Network(object):
         '''
         starting with a shitty implementation working with a special network and we will improve that later
         '''
-        activation = {}
         m = X.shape[0]
+        activation = {}
         for n in range(self.n_lay - 1):
+            X = np.c_[np.ones((X.shape[0], 1)), X]
             activation[n] = X
-            X = functions.sigmoid(np.dot(X, self.weights[n]))
+            X = functions.sigmoid(np.dot(X, self.weights[n].T))
+        print activation[0].shape
+        print activation[1].shape
         delta3 = X - Y
-        delta2 = np.dot(delta3,self.weights[1].T) * functions.sigmoid_grad(activation[1])
-        Delta1 = np.dot(activation[0].T, delta2)
-        Delta2 = np.dot(activation[1].T, delta3)
-        self.weights[0] = (Delta1 / m) - ((lrate / m) * self.weights[0])
-        self.weights[1] = (Delta2 / m) - ((lrate / m) * self.weights[1])
+        delta2 = np.dot(delta3,self.weights[1])# * functions.sigmoid_grad(activation[1])
+        Delta1 = np.dot(delta2.T, activation[0])
+        Delta2 = np.dot(delta3.T, activation[1])
+        #print Delta1.shape
+        #print Delta2.shape
+        '''
+        self.weights[0] = (Delta1 / m) - ((lrate / m) * self.weights[0].T)
+        self.weights[1] = (Delta2 / m) - ((lrate / m) * self.weights[1].T)
+        '''
 
     def evaluate(self, X, Y):
         m = X.shape[0]
         right = 0
         for n in range(self.n_lay - 1):
             X = functions.sigmoid(np.dot(X, self.weights[n]))
-        print X[0]
         #for i in range(m - 1):
         #    np.array_equal(X[i], Y[i])
 
@@ -64,14 +70,11 @@ class Network(object):
         '''
         n = len(data)
         size = int(n * 0.6)
-        print self.cost_function(data[0:n, 0:400], data[0:n, 400:410])
         for i in range(cicles):
             random.shuffle(data)
             X = data[0:size,0:400]
             Y = data[0:size,400:410]
             self.back_prop(X, Y, lrate)
-        self.evaluate(data[size:n,0:400], data[size:n, 400:410])
-        print self.cost_function(X, Y)
     
     def __init__(self, sizes):
         self.n_lay = len(sizes)
--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import random
 import Network
 import scipy.io as sio
 
-net = Network.Network([400, 30, 10])
+net = Network.Network([400, 25, 10])
 data = sio.loadmat('ex4data1.mat')
+theta = sio.loadmat('ex4weights.mat')
+T1 = np.array(theta['Theta1'])
+T2 = np.array(theta['Theta2'])
+net.set_weights(T1, T2)
 X = np.array(data['X'])
 Y = np.array(data['y'])
 y_ = []
 for i in range(len(Y)):
-    y_.append(np.array([[Y[i] % 10 == j] for j in range(10)], dtype=int).T[0][0])
+    y_.append(np.array([[Y[i] == j] for j in range(1, 11)], dtype=int).T[0][0])
 y_ = np.array(y_)
 X_ = np.c_[X, y_]
-net.cost_function(X, y_)
-net.back_prop(X, y_, 0.3)
+print net.cost_function(X, y_)
+net.gradient_descent(X_, 100, 0.1)
+#net.evaluate(X, y_)
