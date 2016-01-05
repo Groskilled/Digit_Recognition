9d46a77af5240c0f70a421db67d3399a03b697c8
Groskilled <adam.wybierala@gmail.com>
2016-01-05 18:01:13 +0000

    cost function ok

--- Network.py
+++ Network.py
@@ -19,14 +19,17 @@ class Network(object):
             ret[i] = np.random.randn(sizes[i], sizes[i + 1])
         return ret
 
+    def set_weights(self, T1, T2):
+        self.weights[0] = T1
+        self.weights[1] = T2
+
     def cost_function(self, X, Y):
-        cost = 0
-        m = X.shape[0]
         for n in range(self.n_lay - 1):
-            X = functions.sigmoid(np.dot(X, self.weights[n]) + self.biases[n].T)
+            X = np.c_[np.ones((X.shape[0], 1)), X]
+            X = functions.sigmoid(np.dot(X, self.weights[n].T))
         left = -Y * np.log(X)
         right = (1 - Y) * np.log(1 - X)
-        tmp = left - right
+        tmp = np.sum(left - right, axis=1)
         return np.mean(tmp)
 
     def back_prop(self, X, Y, lrate):
@@ -37,20 +40,38 @@ class Network(object):
         m = X.shape[0]
         for n in range(self.n_lay - 1):
             activation[n] = X
-            X = functions.sigmoid(np.dot(X, self.weights[n]) + self.biases[n].T)
+            X = functions.sigmoid(np.dot(X, self.weights[n]))
         delta3 = X - Y
         delta2 = np.dot(delta3,self.weights[1].T) * functions.sigmoid_grad(activation[1])
         Delta1 = np.dot(activation[0].T, delta2)
         Delta2 = np.dot(activation[1].T, delta3)
-        self.weights[0] = Delta1 / m + ((lrate / m) * self.weights[0])
-        self.weights[1] = Delta2 / m + ((lrate / m) * self.weights[1])
+        self.weights[0] = (Delta1 / m) - ((lrate / m) * self.weights[0])
+        self.weights[1] = (Delta2 / m) - ((lrate / m) * self.weights[1])
 
-    def gradient_descent(self, data, lrate):
+    def evaluate(self, X, Y):
+        m = X.shape[0]
+        right = 0
+        for n in range(self.n_lay - 1):
+            X = functions.sigmoid(np.dot(X, self.weights[n]))
+        print X[0]
+        #for i in range(m - 1):
+        #    np.array_equal(X[i], Y[i])
+
+    def gradient_descent(self, data, cicles, lrate):
         '''
         data is the whole set of data
         lrate is the learning rate
         '''
         n = len(data)
+        size = int(n * 0.6)
+        print self.cost_function(data[0:n, 0:400], data[0:n, 400:410])
+        for i in range(cicles):
+            random.shuffle(data)
+            X = data[0:size,0:400]
+            Y = data[0:size,400:410]
+            self.back_prop(X, Y, lrate)
+        self.evaluate(data[size:n,0:400], data[size:n, 400:410])
+        print self.cost_function(X, Y)
     
     def __init__(self, sizes):
         self.n_lay = len(sizes)
