750189f6aee0e5377852b47225968088284e35f6
Groskilled <adam.wybierala@gmail.com>
2016-01-10 16:49:14 +0000

    do the evaluate fonction

---
+++ Network.py
@@ -0,0 +1,59 @@
+import functions
+import random
+import numpy as np
+
+class Network(object):
+
+    def feed_forward(self, X):
+        for b, w in zip(self.biases, self.weights):
+            X = np.dot(w.T, X) + b
+        return X
+
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        a = x
+        acts = [x]
+        zs = [] 
+        for b,w in zip(self.biases, self.weights):
+            z = np.dot(w.T, a)
+            zs.append(z)
+            a = functions.sigmoid(z)
+            acts.append(a)
+        delta = (a - y) * functions.sigmoid_grad(z)
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, acts[-2].T).T
+        for i in xrange(2, self.n_lay):
+            z = zs[-i]
+            delta = np.dot(self.weights[-i + 1], delta) * functions.sigmoid_grad(z)
+            nabla_b[-i] = delta
+            nabla_w[-i] = np.dot(delta, acts[-i - 1].T).T
+        return nabla_b, nabla_w
+
+    def update(self, batch, lrate):
+        m = len(batch)
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in batch:
+            dnabla_b, dnabla_w = self.backprop(x,y)
+            nabal_b = [nb + dnb for nb, dnb in zip(nabla_b, dnabla_b)]
+            nabal_w = [nw + dnw for nw, dnw in zip(nabla_w, dnabla_w)]
+        self.weights = [w - lrate / m * nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - lrate/m * nb for b, nb in zip(self.biases, nabla_b)]
+
+    def gradient_descent(self, data, size, cicles, lrate):
+        m = len(data)
+        for i in xrange(cicles):
+            random.shuffle(data)
+            batches = [data[j: j+size] for j in xrange(0,m,size)]
+            for batch in batches:
+                self.update(batch, lrate)
+
+    def evaluate(self, X):
+        test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in X]
+        return sum(int(x == y) for (x, y) in X)
+
+    def __init__(self, sizes):
+        self.n_lay = len(sizes)
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(x, y) for x, y in zip(sizes[:-1], sizes[1:])]
--- main.py
+++ main.py
@@ -1,25 +1,8 @@
-import numpy as np
-import random
+import mnist_loader
 import Network
-import scipy.io as sio
 
-net = Network.Network([400, 25, 10])
-data = sio.loadmat('ex4data1.mat')
-theta = sio.loadmat('ex4weights.mat')
-T1 = np.array(theta['Theta1'])
-T2 = np.array(theta['Theta2'])
-net.set_weights(T1, T2)
-X = np.array(data['X'])
-Y = np.array(data['y'])
-y_ = []
-for i in range(len(Y)):
-    y_.append(np.array([[Y[i] == j] for j in range(1, 11)], dtype=int).T[0][0])
-y_ = np.array(y_)
-X_ = np.c_[X, y_]
-net.evaluate(X,y_)
-net.gradient_descent(X_, 1, 0.01)
-net.gradient_descent(X_, 10, 0.01)
-net.gradient_descent(X_, 100, 0.01)
-net.evaluate(X,y_)
-net.gradient_descent(X_, 1000, 0.01)
-net.evaluate(X,y_)
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net = Network.Network([784, 30, 10])
+X, Y = zip(*training_data)
+net.gradient_descent(training_data, 10, 1, 1)
+net.evaluate(training_data)
