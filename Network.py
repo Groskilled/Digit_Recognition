aca4677f7c7f0860cc815ead1468edc1984697c9
Groskilled <adam.wybierala@gmail.com>
2016-01-11 14:17:50 +0000

    It learns ls!

--- Network.py
+++ Network.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import functions
 import random
 import numpy as np
@@ -16,11 +17,11 @@ class Network(object):
         acts = [x]
         zs = [] 
         for b,w in zip(self.biases, self.weights):
-            z = np.dot(w.T, a)
+            z = np.dot(w.T, a) + b
             zs.append(z)
             a = functions.sigmoid(z)
             acts.append(a)
-        delta = (a - y) * functions.sigmoid_grad(z)
+        delta = (a - y) * functions.sigmoid_grad(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, acts[-2].T).T
         for i in xrange(2, self.n_lay):
@@ -36,22 +37,27 @@ class Network(object):
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in batch:
             dnabla_b, dnabla_w = self.backprop(x,y)
-            nabal_b = [nb + dnb for nb, dnb in zip(nabla_b, dnabla_b)]
-            nabal_w = [nw + dnw for nw, dnw in zip(nabla_w, dnabla_w)]
-        self.weights = [w - lrate / m * nw for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b - lrate/m * nb for b, nb in zip(self.biases, nabla_b)]
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, dnabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, dnabla_w)]
+        self.weights = [w - (lrate/m) * nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - (lrate/m) * nb for b, nb in zip(self.biases, nabla_b)]
 
-    def gradient_descent(self, data, size, cicles, lrate):
+    def gradient_descent(self, data, size, cicles, lrate, test_data=None):
         m = len(data)
+        if test_data: n = len(test_data)
         for i in xrange(cicles):
             random.shuffle(data)
-            batches = [data[j: j+size] for j in xrange(0,m,size)]
-            for batch in batches:
-                self.update(batch, lrate)
+            batches = [data[k: k+size] for k in xrange(0,m,size)]
+            for batch in batches: self.update(batch, lrate)
+            if test_data:
+                print "Epoch {0}: {1} / {2}".format(i, self.evaluate(test_data), n)
 
     def evaluate(self, X):
-        test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in X]
-        return sum(int(x == y) for (x, y) in X)
+        total = 0
+        for x, y in X:
+            if y == np.argmax(self.feed_forward(x)):
+                total = total + 1
+        return total
 
     def __init__(self, sizes):
         self.n_lay = len(sizes)
--- main.py
+++ main.py
@@ -3,6 +3,4 @@ import Network
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 net = Network.Network([784, 30, 10])
-X, Y = zip(*training_data)
-net.gradient_descent(training_data, 10, 1, 1)
-net.evaluate(training_data)
+net.gradient_descent(training_data, 30, 5, 3.0, test_data)
